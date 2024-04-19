4c58d04d7c18a7e8c1a51517f1378edeb58898e1
respectupper <944174864@qq.com>
2024-04-19 16:41:15 +0800

    2024_0419_1641

--- Player/Ball.gd
+++ Player/Ball.gd
@@ -1,22 +1,28 @@
 extends CharacterBody2D
 
+@onready var model : Sprite2D = $Model
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
 var dyna_speed = 0.0;
+
+var dis_move : bool = false
 func _physics_process(delta):
-	# Add the gravity.
-	dyna_speed += delta
-	if not is_on_floor():
-		velocity.y += gravity * delta - dyna_speed
 	
-	if Input.is_action_pressed("ui_up") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	
 	if Input.is_action_just_released("ui_up") :
 		dyna_speed = 0.0
+		
+	if dis_move : return
+	if Input.is_action_pressed("ui_up") :
+		if is_on_floor():
+			velocity.y = JUMP_VELOCITY
+			velocity.y -= dyna_speed
+			dyna_speed = wrap(dyna_speed + 50,0,350)
 	
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
@@ -24,12 +30,21 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	
-	_ratation_ball(direction,delta)
+	_ratation_ball(direction)
 	move_and_slide()
 
-func _ratation_ball(direction : float,delta : float) :
+func _ratation_ball(direction : float) :
 	if direction :
-		rotate(direction * 0.2)
+		model.rotate(direction * 0.2)
 	else :
-		rotate(direction * 0.2)
+		model.rotate(direction * 0.2)
+	pass
+
+var is_big : bool = false
+func _change_size():
+	if is_big :
+		var tween = create_tween()
+		tween.bind_node(self)
+		#tween.tween_property()
+		pass
 	pass
